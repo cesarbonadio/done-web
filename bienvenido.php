682d421be84c7d4a8e4771bf4847376d2d81d5c6
cesarbonadio <32617164+cesarbonadio@users.noreply.github.com>
2017-12-07 15:48:55 -0400

    Update bienvenido.php

--- bienvenido.php
+++ bienvenido.php
@@ -4,6 +4,20 @@
 	<title>Done!</title>
 </head>
 <body>
+	
+	<?php 
+	
+	session_start();
+	
+	if (isset($_SESSION['username'])){
+	  echo 'welcome'.$_SESSION['username'];
+	}else{
+	  header('Location: Inicio.php');
+	}
+	
+	?>
+	
+	
   <h1>Bienvenido a Done</h1>
 </body>
 </html>
